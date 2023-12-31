addc5c19e2a66e6091771f4f7df97e7c64c3602c
Yhago Felipe <yhago.felipe.teles@gmail.com>
2023-12-31 01:05:07 -0300

    Componente Header finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import './App.css'
-import Home from './pages/Home'
+import Header from './components/Header'
 
 function App() {
   return (
     <>
-      <Home />
+      <Header />
     </>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import logo from "../assets/img/logo.png"
+import { TfiSearch } from "react-icons/tfi";
+import { BsFillMoonFill } from "react-icons/bs";
+
+export default function Header(){
+    return(
+        <div className="flex items-center p-3">
+            <img src={logo} width={60} height={60}/>
+            <div className="flex w-full p-2 bg-zinc-500 items-center mx-5 rounded-full">
+                <TfiSearch />
+                <input type="text" placeholder="Procurar Jogo..." className="bg-transparent outline-none px-2 text-white"/>
+            </div>
+            <div>
+                <BsFillMoonFill /> {/*2217*/}
+            </div>
+        </div>
+    )
+}
