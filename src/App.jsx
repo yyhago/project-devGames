0f4045f03e6f6b232cffe81d76861ad63a7a86e6
Yhago Felipe <yhago.felipe.teles@gmail.com>
2024-01-02 14:46:00 -0300

    Dark mode header

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
-import './App.css'
-import Header from './components/Header'
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import { ThemeContext } from "./context/ThemeContext";
+
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [theme, setTheme] = useState("light");
+
   return (
-    <>
-      <Header />
-    </>
-  )
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      <div
+        className={`${theme}
+    ${theme == "dark" ? "bg-[#121212]" : null}`}
+      >
+        <Header />
+        <Home />
+      </div>
+    </ThemeContext.Provider>
+  );
 }
 
-export default App
+export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,46 @@
-import logo from "../assets/img/logo.png"
+import logo from "../assets/img/logo.png";
 import { TfiSearch } from "react-icons/tfi";
-import { BsFillMoonFill} from "react-icons/bs";
+import { BsFillMoonFill } from "react-icons/bs";
 import { WiDaySunny } from "react-icons/wi";
+import { useEffect, useState } from "react";
+import { useContext } from 'react';
+import { ThemeContext } from "../context/ThemeContext";
 
-export default function Header(){
-    return(
-        <div className="flex items-center p-3">
-            <img src={logo} width={60} height={60}/>
-            <div className="flex w-full p-2 bg-zinc-500 items-center mx-5 rounded-full">
-                <TfiSearch />
-                <input type="text" placeholder="Procurar Jogo..." className="bg-transparent outline-none px-2 text-white"/>
-            </div>
-            <div>
-                <BsFillMoonFill /> {/*2217*/}
-                <WiDaySunny />
-            </div>
-        </div>
-    )
+export default function Header() {
+  const [toogle, setToogle] = useState(false);
+  const {theme, setTheme} = useContext(ThemeContext);
+
+  useEffect(() => {
+    console.log("theme", theme);
+  }, []);
+  return (
+    <div className="flex items-center p-3">
+      <img
+        src={logo}
+        width={60}
+        height={60}
+      />
+      <div className="flex w-full p-2 bg-zinc-500 items-center mx-5 rounded-full">
+        <TfiSearch />
+        <input
+          type="text"
+          placeholder="Procurar Jogo..."
+          className="bg-transparent outline-none px-2 text-white"
+        />
+      </div>
+      <div>
+        {theme == 'light' ? (
+          <BsFillMoonFill
+            onClick={() => setTheme('dark')}
+            className="text-[35px] bg-zinc-400 text-black p-1 rounded-full cursor-pointer"
+          />
+        ) : (
+          <WiDaySunny
+            onClick={() => setTheme('light')}
+            className="text-[35px] bg-zinc-300 text-black p-1 rounded-full cursor-pointer"
+          />
+        )}
+      </div>
+    </div>
+  );
 }
